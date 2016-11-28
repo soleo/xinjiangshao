8aace7f3c2cb696d348de8a77a49e4b53edb8d2f
Xinjiang Shao <shaoxinjiang@gmail.com>
2016-11-27 21:28:44 -0600

    Add heap analytics.

--- webpack.conf.js
+++ webpack.conf.js
@@ -8,12 +8,17 @@ export default {
         test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
         loader: "file?name=/[hash].[ext]"
       },
-      {test: /\.json$/, loader: "json-loader"},
       {
-        loader: "babel",
+        test: /\.json$/, loader: "json-loader"
+      },
+      {
+        loader: 'babel',
         test: /\.js?$/,
         exclude: /node_modules/,
-        query: {cacheDirectory: true}
+        query: {
+          presets: ['es2015'],
+          cacheDirectory: true
+        }
       }
     ]
   },
