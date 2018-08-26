cc502036ba02c8d77709e6a8e4a956cc80e186bf
Xinjiang Shao <shaoxinjiang@gmail.com>
2018-08-26 14:52:04 -0500

    Update links for labs.

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,1 @@
-async function getGithubOrgsAsync() {
-    let response = await fetch('https://api.github.com/users/soleo/orgs');
-    let orgs = await response.json();
-    return orgs;
-}
 
-getGithubOrgsAsync()
-    .then(orgs => console.log(orgs))
-    .catch(reason => console.log(reason));
