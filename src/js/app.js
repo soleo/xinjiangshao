7d69e4a9a1cb82bd7dccc7e894a11e7b25269c9c
Xinjiang Shao <shaoxinjiang@gmail.com>
2019-07-27 11:46:15 -0500

    Update home template to support reader mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,3 @@
-!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
-  analytics.load("TizLBwP8We9JqOpeb48BbzCZjyrC4v6h");
-  analytics.page();
-}}();
-
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+console.warn("WARNING: You are using console to inspect my secrets!");
+console.warn("Contact me if you have any questions!");
