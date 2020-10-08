ee5118b06a962089d33431f60290ea7bb5c51ce4
Xinjiang Shao <shaoxinjiang@gmail.com>
2020-10-07 22:02:06 -0400

    Add PostCss config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+// JS Goes here - ES6 supported
+
+import "./css/main.css";
+
 /*eslint no-console: ["error", { allow: ["warn", "error"] }] */
 console.warn("WARNING: You are using console to inspect my secrets!");
 console.warn("Contact me if you have any questions!");
