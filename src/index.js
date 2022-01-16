e95f52b5bf9912d04f5eb1de96a99f3fec0ba822
Xinjiang Shao <xinjiang.shao@hey.om>
2022-01-15 20:42:21 -0600

    Add firebase config and PaperMod theme

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// JS Goes here - ES6 supported
-
-import "./css/main.css";
-
-/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
-console.warn("WARNING: You are using console to inspect my secrets!");
-console.warn("Contact me if you have any questions!");
